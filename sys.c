7cea0ea587cf80d4de5387aeb9f8f0517a425d1f
Buridi Aditya <buridiaditya@gmail.com>
2017-01-17 17:07:12 +0530

    Leave application and acceptance functions

--- sys.c
+++ sys.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define NOOFGUARD 60
+#define NOOFGUARD 150
 #define MAXLEAVES 5
 #define LOGOUT -1
-#define NOOFPLACES 10	
+#define NOOFPLACES 1	
 
 ///// DATATYPE /////
 
@@ -14,10 +14,6 @@ typedef struct{
 	char* name;
 	char* identityNumber;
 	char* password;
-	int day;
-	int time_start;
-	int time_end;
-	int placeIndex;
 	int onLeave;
 	int onOverDuty;
 	int noOfLeaves;
@@ -29,12 +25,14 @@ typedef struct{
 char* Admin = "Buridi";
 char* AdminPassword = "buridi";
 userData* database;
-
+struct tm currentTime;
 int dataBaseSize = 0;
 int noOfLeaveRequests = 0;
 int noOfOverDutyRequests = 0;
 char** leaveRequest;
 char** overDutyRequest;
+char* dutyDiagram[7][3][NOOFPLACES];
+char* days[7] = {"Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday" , "Sunday" };
 
 ///// UTILITY FUNCTION //////
 
@@ -54,6 +52,7 @@ int guardInnerPage(int a,userData* user);
 int adminPage();
 void adminLogin();
 int adminInnerPage();
+void generateSchedule();
 
 /// RUN ////
 
@@ -108,16 +107,21 @@ int main(){
 void guardRegister(){
 	userData user;
 	init(&user);
+	int b;
 	system("clear");
 	printf("Username : ");scanf("%s",user.name);
-	printf("Identity Number : ");scanf("%s",user.identityNumber);
+	printf("UserID : ");scanf("%s",user.identityNumber);
 	printf("Password : ");scanf("%s",user.password);
-	
 	database = realloc(database,(dataBaseSize+1)*sizeof(userData));
-	printf("User Registered Succesfully!\n");
-	system("sleep 1");
 	database[dataBaseSize] = user;
 	dataBaseSize++;
+	printf("User Registered Succesfully!\n");
+	printf("\nPress 1 to return to Previous Page\n");
+	while(1){
+		scanf("%d",&b);
+		if(b == 1) return;
+		else printf("Invalid Input!\n");
+	}
 	return;
 }
 
@@ -148,15 +152,14 @@ int guardPage(userData* user){
 	while(1){
 		system("clear");
 		printf("Welcome , Mr. %s",user->name);
-		printf("\n1. View Next Duty.");
-		printf("\n2. Ask For Leave.");
-		printf("\n3. Ask For Over Duty.");
+		printf("\n1. View Duty.");
+		printf("\n2. Ask For Over Duty.");
 		if(user->onLeave == 0 && user->onOverDuty == 0)
-			printf("\n4. Check Leave/Over Duty request.");
+			printf("\n3. Check Leave/Over Duty request.");
 		else
-			printf("\n4. Check Leave/Over Duty request. -- You have unseen notifications.");
-		printf("\n5. Leave Record Summary.");
-		printf("\n6. LogOut!\n");
+			printf("\n3. Check Leave/Over Duty request. -- You have unseen notifications.");
+		printf("\n4. Leave Record Summary.");
+		printf("\n5. LogOut!\n");
 		scanf("%d",&a);
 		if(guardInnerPage(a,user) == LOGOUT) return LOGOUT;	
 	}
@@ -164,34 +167,50 @@ int guardPage(userData* user){
 }
 
 int guardInnerPage(int a,userData* user){
-	int b;
+	int b,i,j,k;
+	char* leaveDate = (char*)malloc(sizeof(char)*20);
+	char* encryptedDate = (char*)malloc(sizeof(char)*20);
 	while(1){
 		system("clear");
 		switch(a){
-			case 1:			
-				printf("Date Of Duty : %d, StartTime : %d EndTime : %d\n",user->day,user->time_start,user->time_end);
-				printf("Press 1 to return to Previous Page\n");
+			case 1:	
+				for(i = currentTime.tm_wday; i < 7 ; i++){
+					for(j = currentTime.tm_hour / 24; j < 3 ; j++){
+						for(k = 0; k < NOOFPLACES ; k++){
+							if(strcmp(user->name,dutyDiagram[i][j][k]) == 0){
+								printf("Day : %s --- Time : %d:00 - %d:00 --- Location No : %d\n",days[i],(j*8)%24,((j+1)*8)%24,k);
+							}
+						}
+					}
+				}
+				printf("Press 1 to return to Previous Page.\n");
+				printf("Press 2 to apply for leave.\n");
 				while(1){
 					scanf("%d",&b);
 					if(b == 1) return 1;
+					else if(b==2){
+						printf("Enter the Day Number(D),startTime(SS),LocationID in D:SS:LLL\n");
+						scanf(" %s",leaveDate);
+						leaveDate = strcat(leaveDate,user->name);
+						printf("%s",leaveDate);
+						if(noOfLeaveRequests == 0){
+							leaveRequest = (char**)(malloc(sizeof(char*)));
+							noOfLeaveRequests++;
+							leaveRequest[0] = leaveDate;
+						}
+						else{
+							leaveRequest = realloc(leaveRequest,sizeof(char*)*(noOfLeaveRequests+1));
+							leaveRequest[noOfLeaveRequests] = leaveDate;
+							noOfLeaveRequests++;
+						}
+						printf("Applied For Leave!\n");
+						system("sleep 1");
+						system("clear");
+						return 1;
+					}
+					else printf("Invalid Input!");
 				}
-				break;
 			case 2:
-				if(noOfLeaveRequests == 0){
-					leaveRequest = (char**)(malloc(sizeof(char*)));
-					noOfLeaveRequests++;
-					leaveRequest[0] = user->identityNumber;
-				}
-				else{
-					leaveRequest = realloc(leaveRequest,sizeof(char*)*(noOfLeaveRequests+1));
-					leaveRequest[noOfLeaveRequests] = user->name;
-					noOfLeaveRequests++;
-				}
-				printf("Applied For Leave!\n");
-				system("sleep 1");
-				system("clear");
-				return 1;
-			case 3:
 				if(noOfOverDutyRequests == 0){
 					overDutyRequest = (char**)(malloc(sizeof(char*)));
 					noOfOverDutyRequests++;
@@ -206,7 +225,7 @@ int guardInnerPage(int a,userData* user){
 				system("sleep 1");
 				system("clear");
 				return 1;
-			case 4:
+			case 3:
 				if(user->onLeave == 0 && user->onOverDuty == 0){
 					printf("NO Notifications.\n");
 				}
@@ -231,7 +250,7 @@ int guardInnerPage(int a,userData* user){
 					if(b == 1) return 1;
 				}
 				return 1;
-			case 5:
+			case 4:
 				printf("Leave Summary\n");
 				printf("No of Leaves Taken : %d",user->noOfLeaves);
 				printf("\nNo of Pending Leaves : %d",MAXLEAVES - user->noOfLeaves);
@@ -243,7 +262,7 @@ int guardInnerPage(int a,userData* user){
 				system("sleep 1");
 				system("clear");
 				return 1;
-			case 6:
+			case 5:
 				return LOGOUT;
 				break;
 			default :
@@ -261,7 +280,7 @@ int adminPage(){
 		system("clear");
 		printf("1. Create Routine.\n");	
 		printf("2. Manage Leave Requests (%d)\n",noOfLeaveRequests);
-		printf("3. Manage OverDuty Requests (%d)\n",noOfOverDutyRequests);
+		printf("3. Check OverDuty Requests (%d)\n",noOfOverDutyRequests);
 		printf("4. Moniter Locations.\n");
 		printf("5. LogOut!\n");
 		scanf("%d",&a);
@@ -271,12 +290,16 @@ int adminPage(){
 }
 
 int adminInnerPage(int a){
-	int i = 0,b,c;
+	int i = 0,b,c,j,k,d,count,wDay,sTime,Location;
+	char* temp;
 	while(1){
 		system("clear");
 		switch(a){
 			case 1:
-				printf("TODO\n");
+				system("clear");
+				generateSchedule();
+				printf("Schedule Generated!\n");
+				system("sleep 1");
 				return 1;
 			case 2:
 				system("clear");
@@ -287,17 +310,55 @@ int adminInnerPage(int a){
 				}
 				printf("Leave Requests:\n");
 				for( i = 0; i < noOfLeaveRequests; i++){
-					printf("%d. %s\n",i+1,leaveRequest[i]);
+					printf("%d. %s\n",i+1,(leaveRequest[i]+8));
 				}
 				printf("%d. Previous Page\n",noOfLeaveRequests+1);
 				scanf("%d",&b);
 				if(b == noOfLeaveRequests+1) return 0;
 				else{
+					printf("You have %d Guard OverDuty Requests.\n",noOfOverDutyRequests);
 					printf("Press 1 to Approve\nPress 2 do disapprove\n");
 					scanf("%d",&c);
+					b--;
 					if(c == 1){
+						wDay = leaveRequest[b][0]-'0';
+						sTime = ((leaveRequest[b][2]-'0')*10 + (leaveRequest[b][3]-'0'))/8;
+						Location = (leaveRequest[b][5]-'0')*100+(leaveRequest[b][6]-'0')*10+(leaveRequest[b][7]-'0');
 						system("clear");
-						printf("Select a guard Guard to Assign.\n");
+						if(noOfOverDutyRequests != 0){
+							dutyDiagram[wDay][sTime][Location] = overDutyRequest[0];
+							for(int i = 1; i < dataBaseSize; i++){
+								if(strcmp(database[i].name,overDutyRequest[b])==0){
+									database[i].onOverDuty = 1;
+									break;
+								}
+							}
+							for(i=0; i < noOfOverDutyRequests-1; i++){
+					            overDutyRequest[i] = overDutyRequest[i+1];
+					        }
+					        noOfOverDutyRequests--;
+						}
+						else{
+							printf("Select a Guard to Assign.\n");
+							for(i = 1; i < dataBaseSize; i++){
+								if(strcmp(database[i].name,leaveRequest[b]+8) != 0){
+									count++;
+									printf("%d . %s\n",i,database[i].name);
+								}
+							}
+							scanf("%d",&d);
+							dutyDiagram[wDay][sTime][Location] = database[d].name;
+						}
+						for(int i = 1; i < dataBaseSize; i++){
+							if(strcmp(database[i].name,leaveRequest[b]+8)==0){
+								database[i].onLeave = 1;
+								break;
+							}
+						}
+						for(i=b-1; i < noOfLeaveRequests-1; i++){
+				            leaveRequest[i] = leaveRequest[i+1];
+				        }
+				        noOfLeaveRequests--;
 						system("sleep 1");
 					}
 					else{
@@ -312,6 +373,68 @@ int adminInnerPage(int a){
 				        noOfLeaveRequests--;
 					}
 					break;
+				}
+			case 3:
+				system("clear");
+				if(noOfOverDutyRequests == 0){
+					printf("No OverDuty Requests.\n");
+					system("sleep 1");
+					return 1;
+				}
+				printf("OverDuty Requests:\n");
+				for( i = 0; i < noOfOverDutyRequests; i++){
+					printf("%d. %s\n",i+1,overDutyRequest[i]);
+				}
+				printf("%d. Previous Page\n",noOfOverDutyRequests+1);
+				scanf("%d",&b);
+				if(b == noOfOverDutyRequests+1) return 0;
+				if(noOfLeaveRequests == 0){ 
+					printf("No leave requests found. Cannot create an empty Slot for overDuty!\n");
+					system("sleep 1");
+					return 0;
+				}
+				else{
+					printf("Press 1 to Approve\nPress 2 do disapprove\n");
+					scanf("%d",&c);
+					if(c == 1){
+						temp = overDutyRequest[0];
+						overDutyRequest[0] = overDutyRequest[b-1];
+						overDutyRequest[b-1] = temp;
+						printf("You will be redirected to the Leave Requests Page.Kindly accept a leave request to assign overDuty.");
+						a = 2;
+						system("sleep 2");
+					}
+					else{
+						for(i  = 1; i < dataBaseSize; i++){
+							if(strcmp(database[i].name,overDutyRequest[b-1]) == 0){
+								database[i].onOverDuty = -1;
+							}
+						}
+						for(i=b-1; i < noOfOverDutyRequests-1; i++){
+				            overDutyRequest[i] = overDutyRequest[i+1];
+				        }
+				        noOfOverDutyRequests--;
+					}
+					break;
+				}
+			case 4 : 
+				system("clear");
+				for(i = 0; i < 7; i++){
+					printf("Day %d : \n",i+1);
+					for(j = 0; j < 3; j++){
+						if(j==0) printf("00:00-08:00 : \n");
+						if(j==1) printf("08:00-16:00 : \n");
+						if(j==2) printf("16:00-24:00 : \n");
+						for( k = 0;k < NOOFPLACES; k++){
+							printf("  Place %d : %s\n",k,dutyDiagram[i][j][k]);
+						}
+					}
+				}
+				printf("\nPress 1 for previous page\n");
+				while(1){
+					scanf("%d",&b);
+					if(b == 1) return 0;
+					else printf("\nInvalid Input!");
 				}
 			case 5:
 				return LOGOUT;
@@ -341,6 +464,19 @@ void adminLogin(){
 	return;
 }
 
+void generateSchedule(){
+	int count = 0,i,j,k;
+	for(i = 0; i < 7; i++){
+		for(j = 0; j < 3; j++){
+			for(k = 0; k < NOOFPLACES; k++){
+				if(count == dataBaseSize-1) count = 0;
+				dutyDiagram[i][j][k] = database[count+1].name;
+				count++;
+			}
+		}
+	}
+	return;
+}
 //----------------------------//
 
 void init(userData* user){
@@ -351,9 +487,6 @@ void init(userData* user){
 	user->onOverDuty = 0;
 	user->noOfLeaves = 0;
 	user->noOfOverDuty = 0;
-	user->day = 0;
-	user->time_end = 0;
-	user->time_start = 0;
 	return;
 }
 
